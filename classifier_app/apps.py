25e17ebd90e4ada7af6ce3eefec8f0f09f7167e7
mahadaoon6-crypto <mahadaoon6@gmail.com>
2026-09-15 11:08:38 -0800

    Delete classifier_app directory

--- classifier_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ClassifierAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'classifier_app'
--- classifier_app/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def classify_view(request):
-    return render(request, "classifier_app/classify.html")
