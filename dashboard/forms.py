e738aaee2b104ce6465f1c66300d7043eb41f535
mahadaoon6-crypto <mahadaoon6@gmail.com>
2026-09-15 11:08:59 -0800

    Delete dashboard directory

--- dashboard/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class DocumentUploadForm(forms.Form):
-    well_id = forms.CharField(label="رقم البئر", max_length=50)
-    dossier_type = forms.CharField(label="نوع الوثيقة", max_length=50)
-    file = forms.FileField(label="الوثيقة")
